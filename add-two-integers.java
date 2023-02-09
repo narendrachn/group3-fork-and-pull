6b383d7545daa2f66d15110e35b622465ae27340
CVCT121 <CVCT121@nic.bc.ca>
2023-02-09 10:49:32 -0800

    Add scanner functionality
    Adds scanner functionality

--- add-two-integers.java
+++ add-two-integers.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 class Main {
 
     public static void main(String[] args) {
-      
-      int first = 10;
-      int second = 20;
+      Scanner sc = new Scanner (System.in);
+
+      int first = sc.nextInt();
+      int second = sc.nextInt();
   
       // add two numbers
       int sum = first + second;
       System.out.println(first + " + " + second + " = "  + sum);
+      sc.close();
     }
   }
